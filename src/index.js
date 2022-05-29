36fd4e275e326b88092aea4d23f630eea0d446f3
Aliseiko <aliseiko.dev@gmail.com>
2022-05-29 16:00:51 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -3,15 +3,6 @@
 
 module.exports = function towelSort (matrix) {
     if (!matrix) return [];
-    let result = [];
 
-    for (let i = 0; i < matrix.length; i++) {
-        if (i % 2 === 0) {
-            result.push(...matrix[i]);
-        } else {
-            result.push(...matrix[i].reverse());
-        }
-
-    }
-  return result;
+    return matrix.reduce((prev, curr, i) => prev.concat((i % 2 === 0) ? curr : curr.reverse()), []);
 }
